dc42109967410ce827f6e7baa0f5589c7c3ab79b
ercan.duman <ercan.duman@check24.de>
2022-06-24 15:14:13 +0200

    Add design changes in Compose for EventDetailFragment.kt

--- app/src/main/java/com/example/teameventorganiser/presentation/viewmodel/EventDetailViewModel.kt
+++ app/src/main/java/com/example/teameventorganiser/presentation/viewmodel/EventDetailViewModel.kt
@@ -3,4 +3,12 @@ package com.example.teameventorganiser.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 
 class EventDetailViewModel : ViewModel() {
+
+    fun onJoinClicked() {
+
+    }
+
+    fun onViewParticipantsClicked() {
+
+    }
 }
--- app/src/main/java/com/example/teameventorganiser/ui/main/detail/EventDetailFragment.kt
+++ app/src/main/java/com/example/teameventorganiser/ui/main/detail/EventDetailFragment.kt
@@ -4,11 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.teameventorganiser.R
 import com.example.teameventorganiser.presentation.viewmodel.EventDetailViewModel
 
-class EventDetailFragment : androidx.fragment.app.Fragment() {
+class EventDetailFragment : Fragment() {
 
     private val viewModel: EventDetailViewModel by viewModels()
 
@@ -17,7 +38,53 @@ class EventDetailFragment : androidx.fragment.app.Fragment() {
         savedInstanceState: Bundle?
     ): View {
         return ComposeView(requireContext()).apply {
+            setContent {
+                composablePreview()
+            }
+        }
+    }
 
+    @Preview
+    @Composable
+    fun composablePreview() {
+        Column(
+            Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "Event picture",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Title", color = Color.White)
+
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Event Details", color = Color.White)
+
+            Spacer(modifier = Modifier.height(32.dp))
+            OutlinedButton(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp),
+                onClick = { viewModel.onJoinClicked() },
+            ) {
+                Text(text = "Join")
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+            OutlinedButton(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp),
+                onClick = { viewModel.onViewParticipantsClicked() },
+            ) {
+                Text(text = "View Participants")
+            }
         }
     }
 
