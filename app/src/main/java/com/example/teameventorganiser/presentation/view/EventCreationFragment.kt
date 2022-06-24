e77c7ce31d417972c73ae7b82745c278ceb4ef81
moaz.mohamed <moaz.mohamed@check24.de>
2022-06-24 17:18:27 +0200

    WIP event creation fragment

--- app/src/main/java/com/example/teameventorganiser/presentation/view/EventCreationFragment.kt
+++ app/src/main/java/com/example/teameventorganiser/presentation/view/EventCreationFragment.kt
@@ -5,11 +5,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.teameventorganiser.R
 
 class EventCreationFragment : Fragment() {
@@ -34,11 +45,36 @@ class EventCreationFragment : Fragment() {
     fun getEventTitle() {
         var text by remember { mutableStateOf("") }
 
+        // title
         OutlinedTextField(
             value = text,
-            onValueChange = { text = it },
-            label = { text = stringResource(R.string.creation_event_fragment_event_title) }
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            label = { Text(text = resources.getString(R.string.creation_event_fragment_event_title)) },
+            onValueChange = {
+                text = it
+            }
         )
+
+        // description
+        OutlinedTextField(
+            value = text,
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+                .height(200.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            label = { Text(text = resources.getString(R.string.creation_event_fragment_event_description)) },
+            onValueChange = {
+                text = it
+            }
+        )
+
+
+
+
     }
 
 
